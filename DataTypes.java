c95d8c916a422a553c1bda2cd1d3c77e8dac6bbd
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2019-02-07 08:02:12 +0000

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -13,5 +13,15 @@ public class DataTypes{
 		Celsius = ((Fahren - 32) * 5)/9;
 		
 		System.out.println(Fahren + " degree in Fahrenheit is equal to " + Celsius + " in Celsius");
+
+		//one inch is 0.0254m
+		double inches;
+		double centimeters;
+
+		System.out.println("Input a value for inches:");
+		inches = scan.nextDouble();
+		centimeters = inches * 0.0254 * 100;
+
+		System.out.println(inches + " inch(es) is " + centimeters + " centimeters");
 	}
 }
