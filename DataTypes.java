898218bdd19d0b408a4f4e2b7feb118ef40bad1a
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2019-02-08 23:27:38 +0000

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -7,12 +7,15 @@ public class DataTypes{
 		Scanner scan = new Scanner (System.in);
 		double Fahren;
 		double Celsius;
+		double Kelvin;
 		
 		System.out.println("Input a degree in Fahrenheit: ");
 		Fahren = scan.nextDouble();
 		Celsius = ((Fahren - 32) * 5)/9;
+		Kelvin = Celsius + 273.15;
 		
 		System.out.println(Fahren + " degree in Fahrenheit is equal to " + Celsius + " in Celsius");
+		System.out.println("And " + Kelvin + " in Kelvin");
 
 		//one inch is 0.0254m
 		double inches;
