9efc2db6716cdbfdbd8ebfee2e77afbc2822d8bd
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2019-02-27 17:58:47 +0000

    Create RansomNote.java

---
+++ HackerRank/RansomNote.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+
+        HashMap<String, Integer> usableWords = new HashMap<>();
+
+        for (int i = 0; i < magazine.length; i++) {
+            usableWords.merge(magazine[i], 1, Integer::sum);
+        }
+
+        for (int i = 0; i < note.length; i++) {
+            if (usableWords.containsKey(note[i]) && usableWords.get(note[i]) > 0) {
+                usableWords.merge(note[i], -1, Integer::sum); // uses the word
+            } else {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
