2cb02f73cb89bb922b8b053cef1b0d8d4336f283
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-04-10 15:24:10 +0100

    Update newYearChaos.java

--- HackerRank/newYearChaos.java
+++ HackerRank/newYearChaos.java
@@ -1,56 +1,59 @@
-static void minimumSwaps(int[] inputArray) {
-        String answer = "";
-        int totalBribeCount = 0;
-            
-        int length = inputArray.length;
-        HashMap<Integer, Integer> numberToSwapCountMap = new HashMap<Integer, Integer>();
-        boolean hasAllSorted = false;
+public class newYearChaos{
         
-        while(!hasAllSorted) {
-            boolean hasMoreSwaps = false;
-            
-            for(int i = 0; i < length - 1; i++) {
-                if(inputArray[i] > inputArray[i + 1]) {
-                    hasMoreSwaps = true;
-                    // Swap
-                    int temp = inputArray[i];
-                    inputArray[i] = inputArray[i + 1];
-                    inputArray[i + 1] = temp;
+        public static void minimumSwaps(int[] inputArray) {
+                String answer = "";
+                int totalBribeCount = 0;
 
-                    if(!numberToSwapCountMap.containsKey(temp)) {
-                        numberToSwapCountMap.put(temp, 1);
-                    } else {
-                        int swapCount = numberToSwapCountMap.get(temp);
-                        if(swapCount == 2) { 
-                            answer = "Impossible"; 
-                            break; 
+                int length = inputArray.length;
+                HashMap<Integer, Integer> numberToSwapCountMap = new HashMap<Integer, Integer>();
+                boolean hasAllSorted = false;
+
+                while(!hasAllSorted) {
+                    boolean hasMoreSwaps = false;
+
+                    for(int i = 0; i < length - 1; i++) {
+                        if(inputArray[i] > inputArray[i + 1]) {
+                            hasMoreSwaps = true;
+                            // Swap
+                            int temp = inputArray[i];
+                            inputArray[i] = inputArray[i + 1];
+                            inputArray[i + 1] = temp;
+
+                            if(!numberToSwapCountMap.containsKey(temp)) {
+                                numberToSwapCountMap.put(temp, 1);
+                            } else {
+                                int swapCount = numberToSwapCountMap.get(temp);
+                                if(swapCount == 2) { 
+                                    answer = "Impossible"; 
+                                    break; 
+                                }
+                                numberToSwapCountMap.put(temp, swapCount + 1);
+                            }
+                            totalBribeCount++;
+                            //System.out.printf("Index: %d\n", i);
+                            //System.out.printf("Swaps: %d\n", totalBribeCount);
                         }
-                        numberToSwapCountMap.put(temp, swapCount + 1);
+
+                        if(!answer.isEmpty()) {
+                            break;
+                        }
                     }
-                    totalBribeCount++;
-                    //System.out.printf("Index: %d\n", i);
-                    //System.out.printf("Swaps: %d\n", totalBribeCount);
+
+                    if(!hasMoreSwaps) {
+                        hasAllSorted = true;
+                    }
+                    // Print inputArray's state.
+                    /*for(int i = 0; i < length; i++) {
+                        System.out.print(inputArray[i]);
+                        System.out.print(" ");
+                    }
+                    System.out.println();*/
                 }
-                
-                if(!answer.isEmpty()) {
-                    break;
+
+                if(answer.isEmpty()) {
+                    System.out.println(totalBribeCount);
+                } else {
+                    System.out.println(answer);
                 }
             }
-            
-            if(!hasMoreSwaps) {
-                hasAllSorted = true;
-            }
-            // Print inputArray's state.
-            /*for(int i = 0; i < length; i++) {
-                System.out.print(inputArray[i]);
-                System.out.print(" ");
-            }
-            System.out.println();*/
-        }
-        
-        if(answer.isEmpty()) {
-            System.out.println(totalBribeCount);
-        } else {
-            System.out.println(answer);
-        }
-    }
+}
