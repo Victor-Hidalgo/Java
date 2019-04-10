134bba8c2a503d1edadf6028085fd8d9416489de
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-04-10 15:46:34 +0100

    Update newYearChaos.java

--- HackerRank/newYearChaos.java
+++ HackerRank/newYearChaos.java
@@ -1,59 +1,62 @@
 public class newYearChaos{
         
-        public static void minimumSwaps(int[] inputArray) {
-                String answer = "";
-                int totalBribeCount = 0;
+    public static void minimumSwaps(int[] inputArray) {
+        String answer = "";
+        int totalBribeCount = 0;
 
-                int length = inputArray.length;
-                HashMap<Integer, Integer> numberToSwapCountMap = new HashMap<Integer, Integer>();
-                boolean hasAllSorted = false;
+        int length = inputArray.length;
+        HashMap<Integer, Integer> numberToSwapCountMap = new HashMap<Integer, Integer>();
+        boolean allSorted = false;
 
-                while(!hasAllSorted) {
-                    boolean hasMoreSwaps = false;
+        while(!allSorted) {
+            boolean hasMoreSwaps = false;
 
-                    for(int i = 0; i < length - 1; i++) {
-                        if(inputArray[i] > inputArray[i + 1]) {
-                            hasMoreSwaps = true;
-                            // Swap
-                            int temp = inputArray[i];
-                            inputArray[i] = inputArray[i + 1];
-                            inputArray[i + 1] = temp;
+            for(int i = 0; i < length - 1; i++) {
 
-                            if(!numberToSwapCountMap.containsKey(temp)) {
-                                numberToSwapCountMap.put(temp, 1);
-                            } else {
-                                int swapCount = numberToSwapCountMap.get(temp);
-                                if(swapCount == 2) { 
-                                    answer = "Impossible"; 
-                                    break; 
-                                }
-                                numberToSwapCountMap.put(temp, swapCount + 1);
-                            }
-                            totalBribeCount++;
-                            //System.out.printf("Index: %d\n", i);
-                            //System.out.printf("Swaps: %d\n", totalBribeCount);
+                //check if the elements are in ascending order
+                if(inputArray[i] > inputArray[i + 1]) {
+
+                    hasMoreSwaps = true;
+                    // Swap
+                    int temp = inputArray[i];
+                    inputArray[i] = inputArray[i + 1];
+                    inputArray[i + 1] = temp;
+
+                    if(!numberToSwapCountMap.containsKey(temp)) {
+
+                        //add number to the hashmap
+                        numberToSwapCountMap.put(temp, 1);
+                    } 
+                    
+                    else {
+
+                        int swapCount = numberToSwapCountMap.get(temp);
+                        if(swapCount == 2) { 
+                            answer = "too chaotic"; 
+                            break; 
                         }
-
-                        if(!answer.isEmpty()) {
-                            break;
-                        }
+                        numberToSwapCountMap.put(temp, swapCount + 1);
                     }
-
-                    if(!hasMoreSwaps) {
-                        hasAllSorted = true;
-                    }
-                    // Print inputArray's state.
-                    /*for(int i = 0; i < length; i++) {
-                        System.out.print(inputArray[i]);
-                        System.out.print(" ");
-                    }
-                    System.out.println();*/
+                    totalBribeCount++;
+                    
                 }
 
-                if(answer.isEmpty()) {
-                    System.out.println(totalBribeCount);
-                } else {
-                    System.out.println(answer);
+                //stop sorting because the process is "too chaotic"
+                if(!answer.isEmpty()) {
+                    break;
                 }
             }
+
+            
+            if(!hasMoreSwaps) {
+                allSorted = true;
+            }
+        }
+
+        if(answer.isEmpty()) {
+            System.out.println(totalBribeCount);
+        } else {
+            System.out.println(answer);
+        }
+    }
 }
