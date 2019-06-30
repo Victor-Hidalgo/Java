239eb812c6113acef29b2745a95254fb97ca5523
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-06-30 21:41:48 +0100

    Update dateOperations.java

--- dateOperations.java
+++ dateOperations.java
@@ -1,10 +1,14 @@
 import java.util.*;
+import java.io.*;
 
 public class dateOperations{
 
     public static void main(String[] args){
 
         dates date = new dates();
+        Scanner scan = new Scanner (System.in);
+        String strZone = " ";
+        int i = 0;
 
         date.clearCalendar();
         date.setYear(2020);
@@ -12,10 +16,28 @@ public class dateOperations{
         date.setDay(29);
 
         System.out.println("Date: " + date.getYear() + "/" + date.getMonth() + "/" + date.getDay());
-        date.time();
-        date.timeZone("America/New_York");
 
-        //ask the user for the time zone.
+        //print date and time
+        date.time();
+
+        System.out.println("Please enter the time zone");
+
+        strZone = scan.nextLine();
+
+        try{
+
+            Integer.parseInt(strZone);
+            System.out.println("Invalid input");
+            System.exit(0);
+        }
+
+        catch (NumberFormatException e) {
+
+            //deals with any input which is not an integer
+            date.timeZone(strZone);
+        }
+
+        
 
     }
 }
