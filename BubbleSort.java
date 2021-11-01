b52b1222b2aadb1157ce37822142ce1268fd7bbe
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2021-11-01 18:01:25 +0000

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countSwaps' function below.
+     *
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static void countSwaps(List<Integer> a) {
+    // Write your code here
+
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.countSwaps(a);
+
+        bufferedReader.close();
+    }
+}
