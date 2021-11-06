4e4a93e8acdb864b43d34906c40eb2307f22d7cc
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2021-11-06 21:04:34 +0000

    Create Alternation.java

---
+++ HackerRank/Alternation.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'alternatingCharacters' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int alternatingCharacters(String s) {
+    // Write your code here
+        
+        int deletions = 0;
+        StringBuffer sbf = new StringBuffer (s);
+        int i = 0;
+        
+        while(i < sbf.length() - 1){
+            
+            if(sbf.charAt(i) == sbf.charAt(i + 1)){
+                
+                sbf.deleteCharAt(i + 1);
+                deletions++;
+            }
+            i++;
+        }
+        
+        return deletions;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
