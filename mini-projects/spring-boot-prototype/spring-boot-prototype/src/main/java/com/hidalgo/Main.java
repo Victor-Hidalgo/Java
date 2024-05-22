75bf986d6da7dd0e64fdc76cadf696f63abf9217
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-05-22 19:54:55 +0100

    Add files via upload

--- mini-projects/spring-boot-prototype/spring-boot-prototype/src/main/java/com/hidalgo/Main.java
+++ mini-projects/spring-boot-prototype/spring-boot-prototype/src/main/java/com/hidalgo/Main.java
@@ -2,10 +2,18 @@ package com.hidalgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
     }
 }
