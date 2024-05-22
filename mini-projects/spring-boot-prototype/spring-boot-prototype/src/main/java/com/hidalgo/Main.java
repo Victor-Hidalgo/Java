7bd708f36f5bff7f9041fdb3d978837afa5c42f8
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-05-22 23:12:05 +0100

    Add files via upload

--- mini-projects/spring-boot-prototype/spring-boot-prototype/src/main/java/com/hidalgo/Main.java
+++ mini-projects/spring-boot-prototype/spring-boot-prototype/src/main/java/com/hidalgo/Main.java
@@ -1,9 +1,14 @@
 package com.hidalgo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +18,54 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Alex", 28, 30_000)
+        );
+        return response;
     }
+
+    record Person (String name, int age, double savings) {
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hashCode(greet);
+//        }
+//    }
 }
